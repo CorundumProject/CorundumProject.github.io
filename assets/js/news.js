30c99f1dc123676b47c48bd008a6fa0654fd0738
Enio <aielloenio@icloud.com>
2025-06-16 11:35:16 +0200

    Ajout du projet "Fast" dans la navbar et correction de la page d'actualités

---
+++ assets/js/news.js
@@ -0,0 +1,47 @@
+const container = document.querySelector(".news-container");
+let newsCount = 0;
+
+fetch("/assets/json/projects/quick.json")
+    .then(response => {
+        if (!response.ok) {
+            throw new Error("Erreur lors du chargement du fichier JSON");
+        }
+        return response.json();
+    })
+    .then(data => {
+        data.forEach(news => {
+            container.innerHTML = "";
+            if (newsCount < 0) {
+                const col = document.createElement("div");
+                col.classList.add("col");
+
+                const card = document.createElement("div");
+                card.classList.add("card");
+
+                const cardBody = document.createElement("div");
+                cardBody.classList.add("card-body", "text-center");
+
+                const title = document.createElement("h5");
+                title.classList.add("card-title");
+                title.textContent = news.title;
+
+                const description = document.createElement("h6");
+                description.classList.add("card-text");
+                description.textContent = news.description;
+
+                const link = document.createElement("a");
+                link.classList.add("btn", "btn-primary", "mt-2", "mb-2", "w-100");
+                link.textContent = "En savoir plus";
+                link.href = news.link;
+
+                cardBody.appendChild(title);
+                cardBody.appendChild(description);
+                cardBody.appendChild(link);
+                card.appendChild(cardBody);
+                col.appendChild(card);
+                container.appendChild(col);
+                newsCount++;
+            }
+        });
+    })
+    .catch(error => console.error("Erreur :", error));
